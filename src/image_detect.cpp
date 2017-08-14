23c2fabea9abbc7e0d3c5dd49596e9246f5d6499
Jister <chenjiesjtu14@gmail.com>
2017-08-14 20:59:50 +0800

    update image detecting

--- src/image_detect.cpp
+++ src/image_detect.cpp
@@ -20,6 +20,7 @@ private:
 	Mat image_contour;
 public:
 	bool image_valid;
+	bool theta_valid;
 	float ROI_x;
 	float ROI_y;
 	float theta;
@@ -34,7 +35,7 @@ public:
 
 	void getThresholdImage()
 	{
-		GaussianBlur(image, image, Size(7, 7), 0, 0);  
+		GaussianBlur(image, image, Size(5, 5), 0, 0);  
 		for (int i = 0; i < image.rows; i++)
 		{
 			for (int j = 0; j < image.cols; j++)
@@ -52,11 +53,11 @@ public:
 			}
 		}
 		/*image operation*/
-		Mat element = getStructuringElement(MORPH_RECT, Size(4,4));
-		morphologyEx(image_threshold, image_threshold, MORPH_OPEN, element);
-		morphologyEx(image_threshold, image_threshold, MORPH_CLOSE, element);   
-		erode(image_threshold, image_threshold, element);
-		dilate(image_threshold, image_threshold, element);
+		// Mat element = getStructuringElement(MORPH_RECT, Size(4,4));
+		// morphologyEx(image_threshold, image_threshold, MORPH_OPEN, element);
+		// morphologyEx(image_threshold, image_threshold, MORPH_CLOSE, element);   
+		// erode(image_threshold, image_threshold, element);
+		// dilate(image_threshold, image_threshold, element);
 		imshow("image_threshold",image_threshold);
 		waitKey(1);
 	};
@@ -65,10 +66,12 @@ public:
 	{
 		/*find contours*/
 		vector<vector<Point> > contours;
-		vector<Rect> rb;
+		Rect rb;
 		findContours(image_threshold, contours, CV_RETR_EXTERNAL , CV_CHAIN_APPROX_NONE);
 		
-		/*erase unqualified contours*/
+		/*erase unqualified contours, and find the max contour*/
+		float maxarea = 0;  
+		vector<vector<Point> >::iterator maxAreaIdx;  
 		vector<vector<Point> >::iterator it = contours.begin();
 		while(it != contours.end())
 		{
@@ -78,81 +81,158 @@ public:
 			}
 			else
 			{
-				Rect r = boundingRect(Mat(*it));
-				int d = r.width / r.height;
-				if ( d < 0.5 || d > 1.5)
-					it = contours.erase(it);
-				else {
-					rb.push_back(r);
-					rectangle(image, r, Scalar(0, 255, 0), 3);
-					++it;
-				}
+				if(contourArea(*it) > maxarea)
+				{
+					maxarea = contourArea(*it);
+					maxAreaIdx = it;
+				}			
+				++it;
 			}
 		}
 
-		if(rb.size() > 0)
+		if(maxarea > 0)
 		{
 			image_valid = true;
-			ROI_x = (rb[0].x + rb[0].x + rb[0].width) / 2;
-			ROI_y = (rb[0].y + rb[0].y + rb[0].height) / 2;
+			rb = boundingRect(Mat(*maxAreaIdx));
+			rectangle(image, rb, Scalar(0, 255, 0), 3);
+
+			ROI_x = (rb.x + rb.x + rb.width) / 2;
+			ROI_y = (rb.y + rb.y + rb.height) / 2;
 
 			Rect rect;
-			rect.x = ROI_x - rb[0].width/4;
-			rect.y = ROI_y - rb[0].height/4;
-			rect.width = rb[0].width/2;
-			rect.height = rb[0].height/2;
+			rect.x = ROI_x - rb.width/4;
+			rect.y = ROI_y - rb.height/4;
+			rect.width = rb.width/2;
+			rect.height = rb.height/2;
 			Mat ROI_image = Mat(Size(rect.width, rect.height), CV_8UC3);
-
-
 		}else
 		{
 			image_valid = false;
 		}
-
+	
 		if(image_valid)
 		{
 			Mat result(image.size() , CV_8U , cv::Scalar(0)) ; 
 			Mat result_2(image.size() , CV_8U , cv::Scalar(0)) ; 
-			drawContours(result , contours , -1 , cv::Scalar(255) , 3) ;  
+			drawContours(result , contours , -1 , cv::Scalar(255) , 1) ;  
 
-			// vector<cv::Vec2f> lines;
-			// HoughLines(result, lines, 1, CV_PI/180, 150, 0, 0 );  
-			// vector<cv::Vec2f>::const_iterator it_l= lines.begin();
-			// while (it_l!=lines.end())
-			// {
-			// 	float rho= (*it_l)[0];   // 表示距离
-			// 	float theta= (*it_l)[1]; // 表示角度
+			vector<Vec2f> lines;
+			vector<Vec2f> lines_filtered;
+			vector<Vec4i> lines_h; 
+			vector<Vec4i> lines_v; 
+			vector<float> thetas;
+			HoughLines(result, lines, 1, CV_PI/180, 50, 0, 0 );  
+			vector<cv::Vec2f>::const_iterator it_l= lines.begin();
 
-			// 	if (theta < CV_PI/4. || theta > 3*CV_PI/4.) // 若检测为垂直线
-			// 	{ 
-			// 		// 得到线与第一行的交点
-			// 		cv::Point pt1(rho/cos(theta),0);
-			// 		// 得到线与最后一行的交点
-			// 		cv::Point pt2((rho-result_2.rows*sin(theta))/cos(theta),result_2.rows);
-			// 		// 调用line函数绘制直线
-			// 		cv::line(result_2, pt1, pt2, cv::Scalar(255), 1);
-			// 	} else // 若检测为水平线
-			// 	{ 
-			// 		// 得到线与第一列的交点
-			// 		cv::Point pt1(0,rho/sin(theta));
-			// 		// 得到线与最后一列的交点
-			// 		cv::Point pt2(result_2.cols,(rho-result_2.cols*cos(theta))/sin(theta));
-			// 		// 调用line函数绘制直线
-			// 		cv::line(result_2, pt1, pt2, cv::Scalar(255), 1);
-			// 	}
-			// 	++it_l;
-			// }
+			while (it_l!=lines.end())
+			{
+				// ROS_INFO("angle:%d",(int)((*it_l)[1]*57.3));
+				// ROS_INFO("dis:%d",(int)((*it_l)[0]));
+				bool check = false;
+				if(lines_filtered.size() > 0)
+				{
+					vector<cv::Vec2f>::const_iterator ii= lines_filtered.begin();
+					while (ii!=lines_filtered.end())
+					{
+						if(fabs((*ii)[1] - (*it_l)[1]) < CV_PI/4 && fabs((*ii)[0] - (*it_l)[0])<100)
+						{
+							check = true;
+							break;
+						}
+						++ii;
+					}
+				}
 
-			vector<Vec4i> lines;  
-			HoughLinesP(result, lines, 1, CV_PI/180, 100, 20, 100 );  
-			for( size_t i = 0; i < lines.size(); i++ )  
-			{  
-			    line( result_2, Point(lines[i][0], lines[i][1]),  
-			        Point(lines[i][2], lines[i][3]), Scalar(255), 1);  
-			}  
-				 
+				if(!check)
+				{
+					lines_filtered.push_back((*it_l));
+				}
+				
+				++it_l;
+			}
+
+			vector<cv::Vec2f>::const_iterator ii= lines_filtered.begin();
+			while (ii!=lines_filtered.end())
+			{
+				float rho= (*ii)[0];   // 表示距离
+				float theta= (*ii)[1]; // 表示角度
+				Vec4i temp_line;
+
+				if (theta < CV_PI/4. || theta > 3*CV_PI/4.) // 若检测为垂直线
+				{ 
+					// 得到线与第一行的交点
+					Point pt1(rho/cos(theta),0);
+					// 得到线与最后一行的交点
+					Point pt2((rho-result_2.rows*sin(theta))/cos(theta),result_2.rows);
+					// 调用line函数绘制直线
+					line(result_2, pt1, pt2, cv::Scalar(255), 1);
+					temp_line[0] = pt1.x;
+					temp_line[1] = pt1.y;
+					temp_line[2] = pt2.x;
+					temp_line[3] = pt2.y;
+					lines_v.push_back(temp_line);
+					thetas.push_back(theta);
+				} else // 若检测为水平线
+				{ 
+					// 得到线与第一列的交点
+					Point pt1(0,rho/sin(theta));
+					// 得到线与最后一列的交点
+					Point pt2(result_2.cols,(rho-result_2.cols*cos(theta))/sin(theta));
+					// 调用line函数绘制直线
+					line(result_2, pt1, pt2, cv::Scalar(255), 1);
+					temp_line[0] = pt1.x;
+					temp_line[1] = pt1.y;
+					temp_line[2] = pt2.x;
+					temp_line[3] = pt2.y;
+					lines_h.push_back(temp_line);
+				}
+				++ii;
+			}
+
+			vector<Point> corner_point;
+			for(int i = 0; i < lines_v.size(); i++)
+			{
+				for(int j = 0; j < lines_h.size(); j++)
+				{
+					Point pt;	
+					float x0 = lines_v[i][0];
+					float y0 = lines_v[i][1];
+					float x1 = lines_v[i][2];
+					float y1 = lines_v[i][3];
+					float x2 = lines_h[j][0];
+					float y2 = lines_h[j][1];
+					float x3 = lines_h[j][2];
+					float y3 = lines_h[j][3];
+					
+					pt.y = ( (y0-y1)*(y3-y2)*x0 + (y3-y2)*(x1-x0)*y0 + (y1-y0)*(y3-y2)*x2 + (x2-x3)*(y1-y0)*y2 ) / ( (x1-x0)*(y3-y2) + (y0-y1)*(x3-x2) );
+					pt.x = x2 + (x3-x2)*(pt.y-y2) / (y3-y2);
+		
+					corner_point.push_back(pt);
+					circle(image, pt, 10,  Scalar(0,0,255), 2, 8, 0);
+				}
+			}
+
+			float ratio = (float)rb.width / (float)rb.height;
+			if((ratio > 0.95 || ratio < 1.05) && corner_point.size() == 4)
+			{
+				theta_valid = true;
+				theta = (thetas[0] + thetas[1]) /
+			}else
+			{
+				theta_valid = false;
+			}
+			// imshow("counters" , result) ;
+			// vector<Vec4i> lines;  
+			// HoughLinesP(result, lines, 1, CV_PI/180, 50, 30, 50 );  
+			// for( size_t i = 0; i < lines.size(); i++ )  
+			// {  
+			//     line( result_2, Point(lines[i][0], lines[i][1]),  
+			//         Point(lines[i][2], lines[i][3]), Scalar(255), 1);  
+			// }  
+			//ROS_INFO("%d",(int)corner_point.size());	 
 			image_contour = result_2;
-			imshow("resultImage" , image_contour) ;   
+			imshow("src" , image) ;  
+			imshow("line", image_contour);
 			waitKey(1);
 		}
 			
@@ -203,27 +283,27 @@ int main(int argc, char **argv)
 	Mat frame;
 	VideoCapture cap(0);  
 
-    while(!cap.isOpened() && ros::ok())  
-    {  
-        ROS_INFO("Cannot connect to camera......");
-        ros::spinOnce();
+	while(!cap.isOpened() && ros::ok())  
+	{  
+		ROS_INFO("Cannot connect to camera......");
+		ros::spinOnce();
 		loop_rate.sleep();
-    }  
+	}  
 
 	while(ros::ok())
 	{
 		cap>>frame;  
+		//frame = imread("/home/chenjie/catkin_ws/src/project_3/images/4.png");
 		sensor_msgs::ImagePtr msg;  
 		if(!frame.empty())  
 		{  
-			imshow("source", frame);
+			//imshow("source", frame);
 			msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();    
 			image_pub.publish(msg);  
 
 			imageDetect test(frame);
 			test.getThresholdImage();
-			//test.getPosition();
-			//test.getCorner();
+			test.getPosition();
 
 			if(test.image_valid)
 			{
@@ -233,6 +313,7 @@ int main(int argc, char **argv)
 				//img_msg.ROI = cv_bridge::CvImage(std_msgs::Header(), "bgr8", test.image_ROI).toImageMsg();
 				img_msg.x = test.ROI_x;
 				img_msg.y = test.ROI_y;
+				img_msg.theta_valid = test.theta_valid;
 				img_msg.theta = test.theta;
 				img_msg.center_x = frame.rows / 2.0;
 				img_msg.center_y = frame.cols / 2.0;
