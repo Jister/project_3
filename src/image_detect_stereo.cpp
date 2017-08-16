7a9e8cecad4cb5693352e69aaafb3940fa25830d
g-ch <chg10086@yeah.net>
2017-08-16 17:01:31 +0800

    update

--- src/image_detect_stereo.cpp
+++ src/image_detect_stereo.cpp
@@ -24,8 +24,7 @@ private:
 	Mat image_threshold;
 public:
 	bool image_valid;
-	float ROI_x;
-	float ROI_y;
+	Point2f uvPos;
 
 	imageDetect(Mat src)
 	{
@@ -97,19 +96,90 @@ public:
 			rb = boundingRect(Mat(*maxAreaIdx));
 			rectangle(image, rb, Scalar(0, 255, 0), 3);
 
-			ROI_x = (rb.x + rb.x + rb.width) / 2;
-			ROI_y = (rb.y + rb.y + rb.height) / 2;
+			uvPos.x = (rb.x + rb.x + rb.width) / 2;
+			uvPos.y = (rb.y + rb.y + rb.height) / 2;
 
 		}else
 		{
 			image_valid = false;
 		}
 			
-	};
-
-	
-		
+	}		
 };
+
+//************************************  
+// Description: 根据左右相机中成像坐标求解空间坐标  
+// Method:    uv2xyz  
+// FullName:  uv2xyz  
+// Access:    public   
+// Parameter: Point2f uvLeft  
+// Parameter: Point2f uvRight  
+// Returns:   cv::Point3f  
+// Author:    小白  
+// Date:      2017/01/10  
+// History:  
+//************************************  
+Point3f uv2xyz(Point2f uvLeft,Point2f uvRight)  
+{  
+    //  [u1]      |X|                     [u2]      |X|  
+    //Z*|v1| = Ml*|Y|                   Z*|v2| = Mr*|Y|  
+    //  [ 1]      |Z|                     [ 1]      |Z|  
+    //            |1|                               |1|  
+    Mat mLeftRotation = Mat(3,3,CV_32F,leftRotation);  
+    Mat mLeftTranslation = Mat(3,1,CV_32F,leftTranslation);  
+    Mat mLeftRT = Mat(3,4,CV_32F);//左相机M矩阵  
+    hconcat(mLeftRotation,mLeftTranslation,mLeftRT);  
+    Mat mLeftIntrinsic = Mat(3,3,CV_32F,leftIntrinsic);  
+    Mat mLeftM = mLeftIntrinsic * mLeftRT;  
+    //cout<<"左相机M矩阵 = "<<endl<<mLeftM<<endl;  
+  
+    Mat mRightRotation = Mat(3,3,CV_32F,rightRotation);  
+    Mat mRightTranslation = Mat(3,1,CV_32F,rightTranslation);  
+    Mat mRightRT = Mat(3,4,CV_32F);//右相机M矩阵  
+    hconcat(mRightRotation,mRightTranslation,mRightRT);  
+    Mat mRightIntrinsic = Mat(3,3,CV_32F,rightIntrinsic);  
+    Mat mRightM = mRightIntrinsic * mRightRT;  
+    //cout<<"右相机M矩阵 = "<<endl<<mRightM<<endl;  
+  
+    //最小二乘法A矩阵  
+    Mat A = Mat(4,3,CV_32F);  
+    A.at<float>(0,0) = uvLeft.x * mLeftM.at<float>(2,0) - mLeftM.at<float>(0,0);  
+    A.at<float>(0,1) = uvLeft.x * mLeftM.at<float>(2,1) - mLeftM.at<float>(0,1);  
+    A.at<float>(0,2) = uvLeft.x * mLeftM.at<float>(2,2) - mLeftM.at<float>(0,2);  
+  
+    A.at<float>(1,0) = uvLeft.y * mLeftM.at<float>(2,0) - mLeftM.at<float>(1,0);  
+    A.at<float>(1,1) = uvLeft.y * mLeftM.at<float>(2,1) - mLeftM.at<float>(1,1);  
+    A.at<float>(1,2) = uvLeft.y * mLeftM.at<float>(2,2) - mLeftM.at<float>(1,2);  
+  
+    A.at<float>(2,0) = uvRight.x * mRightM.at<float>(2,0) - mRightM.at<float>(0,0);  
+    A.at<float>(2,1) = uvRight.x * mRightM.at<float>(2,1) - mRightM.at<float>(0,1);  
+    A.at<float>(2,2) = uvRight.x * mRightM.at<float>(2,2) - mRightM.at<float>(0,2);  
+  
+    A.at<float>(3,0) = uvRight.y * mRightM.at<float>(2,0) - mRightM.at<float>(1,0);  
+    A.at<float>(3,1) = uvRight.y * mRightM.at<float>(2,1) - mRightM.at<float>(1,1);  
+    A.at<float>(3,2) = uvRight.y * mRightM.at<float>(2,2) - mRightM.at<float>(1,2);  
+  
+    //最小二乘法B矩阵  
+    Mat B = Mat(4,1,CV_32F);  
+    B.at<float>(0,0) = mLeftM.at<float>(0,3) - uvLeft.x * mLeftM.at<float>(2,3);  
+    B.at<float>(1,0) = mLeftM.at<float>(1,3) - uvLeft.y * mLeftM.at<float>(2,3);  
+    B.at<float>(2,0) = mRightM.at<float>(0,3) - uvRight.x * mRightM.at<float>(2,3);  
+    B.at<float>(3,0) = mRightM.at<float>(1,3) - uvRight.y * mRightM.at<float>(2,3);  
+  
+    Mat XYZ = Mat(3,1,CV_32F);  
+    //采用SVD最小二乘法求解XYZ  
+    solve(A,B,XYZ,DECOMP_SVD);  
+  
+    //cout<<"空间坐标为 = "<<endl<<XYZ<<endl;  
+  
+    //世界坐标系中坐标  
+    Point3f world;  
+    world.x = XYZ.at<float>(0,0);  
+    world.y = XYZ.at<float>(1,0);  
+    world.z = XYZ.at<float>(2,0);  
+  
+    return world;  
+}  
 
 void leftImageCallback(const sensor_msgs::Image &msg)
 {
@@ -125,9 +195,7 @@ void leftImageCallback(const sensor_msgs::Image &msg)
 		ROS_ERROR("cv_bridge exception: %s", e.what());
 		left_flag = false;
 		return;
-	}
-
-	
+	}	
 }
 
 void rightImageCallback(const sensor_msgs::Image &msg)
